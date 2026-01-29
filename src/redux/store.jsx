00b5ba1901040b5b473c6f58713ffa77a90fe031
achraf-fergal <farkalachraf@gmail>
2026-01-29 15:41:56 +0100

    Updates

--- src/redux/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import reducer from './redus';
-const store = configureStore({
-    reducer: {
-        donner: reducer
-    }
-});
-export default store;
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/3_projet-r-seaux-sociaux/'
-})
